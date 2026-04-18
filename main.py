88a4d8375484141a4cbc9a9ff9575214632c13e8
mp4502 <529866168@qq.com>
2026-04-18 14:10:21 +0800

    初始化及指令测试

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
+import astrbot.api.message_components as Comp
 
-@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
+@register("laizhi", "mp4502", "AstrBot 来只图库插件", "1.0.0")
 class MyPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
@@ -19,6 +20,16 @@ class MyPlugin(Star):
         message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
         logger.info(message_chain)
         yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+        Comp.Image.fromFileSystem("path/to/image.jpg").send(event) # 发送一条图片消息
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
+
+    @filter.regex(r"^来只\s+(\w+)$")
+    async def handle_deer_checkin(self, event: AstrMessageEvent):
+        if(event.message_str.startswith("来只")):
+            keyword = event.message_str.split()[1]
+            # 在这里处理关键词，获取相关图片等
+            yield event.plain_result(f"正在为您搜索与 {keyword} 相关的图片...")
+
+        
